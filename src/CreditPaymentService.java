8a2b56d7e27b86c2a140ee1e75767a1d77a0e36f
Evgesha <Slav.novikova2014@gmail.com>
2023-05-06 16:29:40 +0700

    add src/CreditPaymentService.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,7 +2,11 @@ public class CreditPaymentService {
     public double calculate(double s, double r, double n) {
         double result;
         result = s * ((r / 1200) + ((r / 1200) / (Math.pow((1 + (r / 1200)), n) - 1)));
+        // s- Сумма кредита;
+        // r - Месячная процентная ставка
+        // n- Количество месяцев, на которое вы берете деньги
         return result;
+
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double s; //Сумма кредита
-        double r; // Месячная процентная ставка
-        double n; // Количество месяцев, на которое вы берете деньги
-
 
         System.out.println("Аннуитетные платежи:");
-        System.out.println((int)(service.calculate(1_000_000, 9.99, 12)));
+        System.out.println((int) (service.calculate(1_000_000, 9.99, 12)));
         System.out.println((int) (service.calculate(1_000_000, 9.99, 24)));
         System.out.println((int) (service.calculate(1_000_000, 9.99, 36)));
+
+
     }
 }
