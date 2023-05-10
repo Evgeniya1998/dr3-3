bbff90d4d5af912a47f11dd7f48134e7c34964ca
Evgesha <Slav.novikova2014@gmail.com>
2023-05-10 10:34:45 +0700

    added CreditPaymentService

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
     public double calculate(double amountOfCredit, double monthlyInterestRate, double numberOfMonths) {
         double result;
-        result = s * ((r / 1200) + ((r / 1200) / (Math.pow((1 + (r / 1200)), n) - 1)));
-        // mountOfCredit - Сумма кредита;
+        result = amountOfCredit * ((monthlyInterestRate / 1200) + ((monthlyInterestRate / 1200) / (Math.pow((1 + (monthlyInterestRate / 1200)), numberOfMonths) - 1)));
+        // amountOfCredit - Сумма кредита;
         // monthlyInterestRate - Месячная процентная ставка
         // numberOfMonths - Количество месяцев, на которое вы берете деньги
         return result;
